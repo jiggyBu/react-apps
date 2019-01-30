5a1ab065910f72b352d9f2c53df51de1aea1d240
igorstric <igor.stric@gmail.com>
2019-01-30 21:42:28 +0100

    folder correction track

--- star-wars-app/src/index.js
+++ star-wars-app/src/index.js
@@ -10,6 +10,8 @@ import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
+// test commit
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
